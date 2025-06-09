318aa992e812b71c0fff2d82fda9cea16e7e9bfa
Cocorico03 <129895791+Cocorico03@users.noreply.github.com>
2025-06-09 00:38:13 -0400

    Set homepage to load on '/'

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
     <Router>
       <Navbar/>
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/Home' element={<Home />}/>
         <Route path='/AboutUs' element={<AboutUs/>}/>
         <Route path='/Services' element={<Services/>}/>
